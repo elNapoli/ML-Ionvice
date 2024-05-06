561866f7d7bda66c63992eea2fc6e90f84002277
elnapoli <baldomero.napoli@gmail.com>
2024-05-05 19:48:16 -0500

    se agregan validaciones

--- presenter/src/main/java/com/baldomeronapoli/mlinvoice/presenter/ui/features/history/screens/HistoryFileScreen.kt
+++ presenter/src/main/java/com/baldomeronapoli/mlinvoice/presenter/ui/features/history/screens/HistoryFileScreen.kt
@@ -1,22 +1,34 @@
 package com.baldomeronapoli.mlinvoice.presenter.ui.features.history.screens
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Camera
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.State
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import coil.compose.rememberAsyncImagePainter
 import coil.request.ImageRequest
 import coil.transform.RoundedCornersTransformation
+import com.baldomeronapoli.mlinvoice.presenter.R
 import com.baldomeronapoli.mlinvoice.presenter.components.LoadingDialog
 import com.baldomeronapoli.mlinvoice.presenter.state.BaseUiState
 import com.baldomeronapoli.mlinvoice.presenter.ui.viewmodels.storage.StorageContract
@@ -30,13 +42,30 @@ fun HistoryFileScreen(
         onIntent(StorageContract.Intent.GetAllFiles)
     }
     LoadingDialog(state.value.files is BaseUiState.Loading)
-    state.value.files.data?.let { list ->
+    val files = state.value.files.data
+    if (files.isNullOrEmpty()) {
+        Column(
+            modifier = Modifier.fillMaxSize().alpha(0.5f),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            Icon(
+                modifier = Modifier.size(300.dp),
+                imageVector = Icons.Filled.Camera,
+                contentDescription = null
+            )
+            Text(
+                text = stringResource(id = R.string.empty_files),
+                style = MaterialTheme.typography.headlineLarge
+            )
+        }
+    } else {
         LazyVerticalGrid(
             columns = GridCells.Fixed(2),
             modifier = Modifier.fillMaxSize()
         ) {
-            items(list.size) { index ->
-                val imageUrl = list[index]
+            items(files.size) { index ->
+                val imageUrl = files[index]
                 CoilImage(
                     imageUrl = imageUrl,
                     contentDescription = null,
@@ -48,6 +77,7 @@ fun HistoryFileScreen(
             }
         }
     }
+
 }
 
 @Composable
